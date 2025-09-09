1864873d4727ca49dc3dfced49fee59f68cccb94
Dongyeon Min <dongyeon.min@woowahan.com>
2025-09-09 23:22:13 +0900

    인수 테스트 구현 및 유틸성 함수 구현

---
+++ src/test/java/com/camping/admin/client/ReservationClient.java
@@ -0,0 +1,50 @@
+package com.camping.admin.client;
+
+import com.camping.admin.CommonContext;
+import com.camping.admin.dto.ReservationResponse;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import java.util.List;
+import java.util.Optional;
+
+import static io.restassured.RestAssured.given;
+
+public class ReservationClient {
+    public ReservationResponse getReservation(Long reservationId) {
+        ExtractableResponse<Response> extracted = given().log().all()
+            .contentType("application/json")
+            .header("Authorization", "Bearer " + CommonContext.accessToken)
+            .when().log().all()
+            .get("admin/reservations")
+            .then().log().all()
+            .statusCode(200)
+            .extract();
+
+        List<ReservationResponse> resList = extracted.body().jsonPath().getList("", ReservationResponse.class);
+        System.out.println("### resList = " + resList);
+
+        Optional<ReservationResponse> maybeReservation = resList.stream()
+            .filter(r -> r.getId().equals(CommonContext.reservationId))
+            .findFirst();
+
+        if(maybeReservation.isPresent()) {
+            return maybeReservation.get();
+        } else {
+            throw new RuntimeException("예약이 존재하지 않습니다.");
+        }
+    }
+
+    public List<ReservationResponse> getReservations() {
+        ExtractableResponse<Response> extracted = given().log().all()
+            .contentType("application/json")
+            .header("Authorization", "Bearer " + CommonContext.accessToken)
+            .when().log().all()
+            .get("admin/reservations")
+            .then().log().all()
+            .statusCode(200)
+            .extract();
+
+        return extracted.body().jsonPath().getList("", ReservationResponse.class);
+    }
+}
